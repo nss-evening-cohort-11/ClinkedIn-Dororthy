ea963f5fb05e1a5358b40a75967d70957dbc9fc9
Liza Star <lizadstar18@gmail.com>
2020-09-08 20:53:00 -0500

    set up files/folder structure

---
+++ ClinkedIn-Dorothy/Data/MemberRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedIn_Dorothy.Models;
+
+namespace ClinkedIn_Dorothy.Data
+{
+    public class MemberRepository
+    {
+        static List<Member> _members = new List<Member>();
+
+        public List<Member> GetAll()
+        {
+            return _members;
+        }
+    }
+}
---
+++ ClinkedIn-Dorothy/Models/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn_Dorothy.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Interest { get; set; }
+        public List<string> Services { get; set; }
+        public List<Member> Friends { get; set; }
+        public List<Member> Enemies { get; set; }
+
+    }
+}
